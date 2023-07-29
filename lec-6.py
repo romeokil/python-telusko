5f643d7c1d1cf55779f40c01391e99c4a710b0b5
romeokil <123149490+romeokil@users.noreply.github.com>
2023-07-29 11:53:57 +0530

    working with tuples in python

---
+++ lec-6.py
@@ -0,0 +1,14 @@
+#Tuple Set in Python
+#Tuple is immutable.
+tuple=(33,24,55,67)#Same as list can be represented as square braces or normal braces.
+print(tuple)
+print(tuple.count(33))#No of occurence in the list.
+print(tuple.index(55))#Index of the element.
+#Set bhi same hi hai but isme kuch features hai.
+#Esme indexing kaam me ni aata hai.
+#Esme koi sequencing ni hota hai jitna baar print krega utna baar utna baar alg alg sequence dikhaega.
+#or isme repetition of element show ni krega
+#Based on Hashmaps.
+#Symbol bhi maths wala hi hai.
+s={33,44,23,78,21,55,21}
+print(s)
