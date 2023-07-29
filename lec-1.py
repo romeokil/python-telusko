7ec5e64e8ab5b4f6010b7a3bea35e2236b03f0d4
romeokil <123149490+romeokil@users.noreply.github.com>
2023-07-29 11:51:59 +0530

    handling special characters
    learn to 
    print \n or : under the braces

---
+++ lec-1.py
@@ -0,0 +1,13 @@
+#Introduction to Python 
+print("Hello Rahul Welcome to the Python World")
+print('This is the first lecture using single coats')
+print("rahul"*10)
+print("navin's daughter")
+
+print('c:\docs\navin')
+# There is \n which is basically next line so if we want to print \n what we can do is print it in raw form by attaching r
+print(r'c:\docs\navin')
+# print('navin's "laptop')
+# This line will give error because because we are using single coats
+# if we want to print 's the we have to ignore this. for that we can put \(blackslash). 
+print('navin\'s "laptop"')
