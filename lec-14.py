bb328f7c1e86174290cc1a26a9ecd3b02fb3814e
romeokil <123149490+romeokil@users.noreply.github.com>
2023-07-29 11:56:42 +0530

    learing different maths module in python

---
+++ lec-14.py
@@ -0,0 +1,15 @@
+#This is lecture no 14 read about different maths function
+'''
+import math as m #if we want all the function from maths module
+print(m.pow(2,5))
+print(m.sqrt(25))
+print(m.factorial(6))
+print(m.floor(6.9))
+print(m.ceil(6.9))
+print(m.pi)
+print(m.e)
+'''
+
+from math import sqrt,pow  #Esme math. sb likhne ka jarurat ni hai directly use kr skta hai
+print(sqrt(34))
+print(pow(3,2))
