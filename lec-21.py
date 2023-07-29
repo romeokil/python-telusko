158960d7939ecf7b73cecb3a8afd4badb973c618
romeokil <123149490+romeokil@users.noreply.github.com>
2023-07-29 12:00:05 +0530

    some pattern

---
+++ lec-21.py
@@ -0,0 +1,43 @@
+#This is lecture no 21
+'''
+# # # #
+# # # #
+# # # #
+# # # #
+
+This Pattern we will have to implement
+'''
+'''
+for i in range(4):
+    for j in range(4):
+        print("#",end=" ")
+    print()
+
+'''
+'''
+#
+# #
+# # #
+# # # #
+
+'''
+'''
+for i in range(4):
+    for j in range(i+1):
+        print("#",end=" ")
+    print()
+
+'''
+'''
+# # # #
+# # #
+# #
+#
+'''
+'''
+for i in range(4):
+    for j in range(4-i):
+       print("#",end="")
+    print()
+
+'''
