722e791218a5b318c4d7a2891b923fd14510db45
romeokil <123149490+romeokil@users.noreply.github.com>
2023-07-29 11:57:31 +0530

    taking input in python

---
+++ lec-16.py
@@ -0,0 +1,7 @@
+#User input in Python
+# x=int(input("Enter the first number"))
+# y=int(input("Enter the second number"))
+# print("The result is",(x+y))
+#eval built in function
+x=eval(input("Enter the expression"))
+print("The answer is ",x)
