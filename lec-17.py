2015c887b3bbc03582252f12b1c19b2064fc070d
romeokil <123149490+romeokil@users.noreply.github.com>
2023-07-29 11:58:06 +0530

    working with if and elif

---
+++ lec-17.py
@@ -0,0 +1,22 @@
+#If-elif-else
+#x=int(input("Enter the number to check"))
+#if x>5:
+#    print("Its Greater than 5")
+#elif x>0 and  x<5:
+#    print("Number lies in between 0 and 5")
+#else:
+#    print("Less than 0") 
+
+x=int(input('Enter the number to see in alphabets'))
+if x==0:
+    print("Zero")
+elif x==1:
+    print("One")
+elif x==2:
+    print("Two")
+elif x==3:
+    print("Three")
+elif x==4:
+    print("Four")
+else:
+    print("Greater than 4")
