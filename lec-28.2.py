ced71538093a9f63e2b2c4d1092895040a19d2b4
romeokil <123149490+romeokil@users.noreply.github.com>
2023-07-31 16:29:45 +0530

    Multilevel inheritance

---
+++ lec-28.2.py
@@ -0,0 +1,38 @@
+#Inheritance in oops in pyhton 
+#This is Multi level inheritance
+class A:
+    def feature1(self):
+        print("This is feature 1")
+
+    def feature2(self):
+        print("This is feature 2")
+
+class B(A): #Here we are passing the class A i.e doing inheritance
+    def feature3(self):
+        print("This is feature 3")
+
+    def feature4(self):
+        print("This is feature 4")
+
+class C(B): #Here we are passing class B i.e doing inheritance,since B is also inherting we also get the properties of inherited class
+    def feature5(self):
+        print("This is feature 5")
+print("Accessing the features from object a")
+a=A()
+a.feature1()
+a.feature2()
+
+print("Accessing the features from object b")
+b=B()
+b.feature3()
+b.feature4()
+b.feature1()   #That y we are able to access the feature2 and feature1
+b.feature2()   #Otherwise we are not able to access.
+
+print("Accessing the features from object c")
+c=C()
+c.feature1()
+c.feature2()
+c.feature3()
+c.feature4()
+c.feature5()
